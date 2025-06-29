4057e5351f6756b4776daec8a857ccee70e6c3c3
Engr James <hexxondiv@gmail.com>
2025-06-29 18:37:57 +0100

    Correct string function call

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main() {
     char name[50];
     int age = get_int("Enter your age: ");
-    get_string("Enter your name: ", name, sizeof(name));
+    get_string("Enter your name: ");
     printf("Hello, %s! You are %d years old.\n", name, age);
     return 0;
 }
